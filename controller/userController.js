b1a7a0fac22ed37c7e6df0cade23dc7d08bb70ee
Fannandya Sutan <sfannandya@gmail.com>
2026-07-27 10:14:19 +0700

    membuat function registrasi

---
+++ controller/userController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { where } = require("sequelize");
+
+const User = db.User;
+
+async function register(req, res) {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({message: "email dan password wajib diisi."});
+        }
+
+        const existingUser = await User.findOne({
+            where: {email}
+        });
+
+        if (existingUser) {
+            return res.status(409).json({message: "email sudah terdaftar."});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            email,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({message: "alhamdulillah registrasi berhasil",
+            data: {
+                id: user.id,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
